1a42a29b877aa01afa583d7634b856309961ccdd
Michael Lee <leecommamichael@gmail.com>
2020-05-10 00:45:32 -0500

    Examples!

---
+++ examples/persistence/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "io/ioutil"
+
+func main() {
+	newFilename := "./memory.txt"
+	poem := "Old pond\nA frog jumps in –\nThe sound of water"
+	bytes := []byte(poem)
+	ioutil.WriteFile(newFilename, bytes, 0644)
+}
--- persistence/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "io/ioutil"
-
-func main() {
-	bytes := []byte("I'd like to remember this moment.")
-	ioutil.WriteFile("./hey", bytes, 0644)
-}
